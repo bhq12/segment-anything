9ae5896365a7ef0a2d76153d17f2f5c51544dbbf
Mr Brook Queree <s4842338@login0.compute.eait.uq.edu.au>
2024-08-07 13:12:57 +1000

    Slurming it

---
+++ slurm_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+time=0-00:30:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --job-name="Brook Job"
+#SBATCH --mail-user=s4842338@student.uq.edu.au
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH —output=output_dir/%j.out
+#module load tensorflow/1.9.0
+#export PYTHONPATH=~/.local/lib/python3.6/site-packages/
+export PYTHONPATH=/home/Student/s4842338/.local/lib/miniconda3/envs/segment_anything_1/lib/python3.10/site-packages/
+#export LD_PRELOAD=/home/Student/s4842338/.local/lib/miniconda3/envs/segment_anything_1/lib/python3.10/site-packages/nvidia/cudnn/lib/libcudnn.so.9
+#cd ../checkpoints
+#./download_ckpts.sh
+lspci | grep -i nvidia
+conda activate segment_anything_1
+#python run_sam.py
+python --version
+pip --version
+python run_model.py
