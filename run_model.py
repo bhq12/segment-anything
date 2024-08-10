6f011c9df7a06a65cf5f1316e816e4865f3d5d24
Mr Brook Queree <s4842338@login0.compute.eait.uq.edu.au>
2024-08-10 19:44:14 +1000

    Add slurms

--- run_model.py
+++ run_model.py
@@ -1,2 +1,23 @@
-from segment_anything import sam_model_registry
+from segment_anything import SamPredictor, sam_model_registry
+import cv2
+import time
+
 sam = sam_model_registry["default"](checkpoint="./sam_vit_h_4b8939.pth")
+
+img = cv2.imread("/home/Student/s4842338/dog.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+start = time.time()
+predictor = SamPredictor(sam)
+end = time.time()
+print(f"instantiate predictor time: {end - start}")
+predictor.set_image(img)
+end = time.time()
+print(f"set_image time: {end - start}")
+for i in range(1000):
+    masks, _, _ = predictor.predict()
+end = time.time()
+print(f"masks time: {end - start}")
+end = time.time()
+print("DONE!")
+print(f"time: {end - start}")
